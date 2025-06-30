4c88b19bc9aab99c0e93ff5e5e2dd7b3440d189f
Andre Tavares <andretavares16@gmail.com>
2025-06-30 15:28:38 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeroSectionComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
