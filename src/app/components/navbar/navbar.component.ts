1cd137038907a259f6f8dc5d6b5f51752599832c
Andre Tavares <andretavares16@gmail.com>
2025-07-02 00:13:40 -0300

    responsividades

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   menuAberto = false;
 
-  toggleMenu(): void {
+  toggleMenu() {
     this.menuAberto = !this.menuAberto;
+    document.body.style.overflow = this.menuAberto ? 'hidden' : 'auto';
+    console.log('Menu está', this.menuAberto ? 'aberto' : 'fechado');
   }
 }
